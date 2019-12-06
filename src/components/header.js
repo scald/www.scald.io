4db2ba1acb4bc30bfe8bb8978cab61473994797e
Steve Caldwell <scaldwell@springbuk.com>
2019-12-06 15:55:32 -0500

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,23 +5,25 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#fff`,
       marginBottom: `1.45rem`,
+      borderBottom: `1px solid #242424`
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `.55rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#242424`,
             textDecoration: `none`,
+            fontSize: `14px`
           }}
         >
           {siteTitle}
